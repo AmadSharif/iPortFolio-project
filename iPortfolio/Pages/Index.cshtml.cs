65ce5bc1602ed9908dea2558f4dbd5faf11bd889
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-11-05 05:43:50 -0800

    Connecting front panel with admin panel in index

--- iPortfolio/Pages/Index.cshtml.cs
+++ iPortfolio/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using iPortfolio.Data;
+using iPortfolio.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,27 @@ namespace iPortfolio.Pages
 {
     public class IndexModel : PageModel
     {
+        AppDbContext db;
+        public Profile? profile { get; set; }
+        public Facts facts { get; set; }
+        public List<Skill> skill { get; set; }
+        public List<Education> education { get; set; }
+        public List<Experiance> experiance { get; set; }
+        public List<Service> service { get; set; }
+        public List<Testimonial> testimonial { get; set; }
+        public IndexModel(AppDbContext _db)
+        {
+            db = _db;
+        }
         public void OnGet()
         {
+            profile = db.tbl_Profile.FirstOrDefault();
+            facts = db.tbl_Facts.FirstOrDefault();
+            skill = db.tbl_Skill.ToList();
+            education = db.tbl_Education.ToList();
+            experiance = db.tbl_Experiance.ToList();
+            service = db.tbl_Service.ToList();
+            testimonial = db.tbl_Testimonial.ToList();
         }
     }
 }
