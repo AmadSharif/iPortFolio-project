4dce49eddd2f184a7fdab114111571ee5ede91d0
Amad Sharif <139844237+AmadSharif@users.noreply.github.com>
2023-11-07 13:46:30 -0800

    profile validation update

--- iPortfolio/Model/Profile.cs
+++ iPortfolio/Model/Profile.cs
@@ -1,21 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace iPortfolio.Model
 {
     public class Profile
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter Name")]
         public string Name { get; set; }
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Please Enter your Birth Date")]
         public string Birthday { get; set; }
+        [Required(ErrorMessage = "Please Enter Age")]
         public int Age { get; set; }
+        [Required(ErrorMessage = "Please Enter Website")]
+
         public string Website { get; set; }
+        [Required(ErrorMessage = "Please Enter Degree")]
+
         public string Degree { get; set; }
+        [Phone (ErrorMessage = "Please Enter Phone")]
+
         public string Phone { get; set; }
+        [EmailAddress(ErrorMessage = "Please Enter Email")]
+
         public string EMail { get; set; }
+        [Required(ErrorMessage = "Please Enter City")]
+
         public string City { get; set; }
+        [Required(ErrorMessage = "Please Enter Freelance Status")]
+
         public string FreelanceStatus { get; set; }
+        [Required(ErrorMessage = "Please Enter Description One")]
+
         public string DescOne { get; set; }
+        [Required(ErrorMessage = "Please Enter Description Two")]
+
         public string DescTwo { get; set; }
+
         public string? Image { get; set; }
 
         [NotMapped]
--- iPortfolio/Pages/myadmin/AddProfile.cshtml.cs
+++ iPortfolio/Pages/myadmin/AddProfile.cshtml.cs
@@ -20,17 +20,22 @@ namespace iPortfolio.Pages.myadmin
         public void OnGet()
         {
         }
-        public void OnPost(Profile profile)
+        public IActionResult OnPost(Profile profile)
         {
-            string ImageName = profile.Photo.FileName.ToString();
-            var FolderPath = Path.Combine(env.WebRootPath, "images");
-            var ImagePath = Path.Combine(FolderPath, ImageName);
-            FileStream fs = new FileStream(ImagePath, FileMode.Create);
-            profile.Photo.CopyTo(fs);
-            fs.Dispose();
-            profile.Image = ImageName;
-            db.tbl_Profile.Add(profile);
-            db.SaveChanges();
+            if (ModelState.IsValid) 
+            {
+                string ImageName = profile.Photo.FileName.ToString();
+                var FolderPath = Path.Combine(env.WebRootPath, "images");
+                var ImagePath = Path.Combine(FolderPath, ImageName);
+                FileStream fs = new FileStream(ImagePath, FileMode.Create);
+                profile.Photo.CopyTo(fs);
+                fs.Dispose();
+                profile.Image = ImageName;
+                db.tbl_Profile.Add(profile);
+                db.SaveChanges();
+            }
+            return Page();
+            
 
 
             
